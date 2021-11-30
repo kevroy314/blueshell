91f1dc6bbf44b8b23522077bf01b60ced184c2f1
Kevin Horecka <kevin.horecka@gmail.com>
2021-11-29 18:58:45 -0600

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+ 
+namespace blueshell
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string[] Args;
+        public static Dictionary<string, object> parsedArgs;
+        void app_Startup(object sender, StartupEventArgs e)
+        {
+            // If no command line arguments were provided, don't process them if (e.Args.Length == 0) return;  
+            if (e.Args.Length > 0)
+            {
+                Args = e.Args;
+                parsedArgs = new Dictionary<string, object>();
+                parsedArgs.Add("mute", false);
+                parsedArgs.Add("src", Args[Args.Length-1]);
+                parsedArgs.Add("opacity", 1f);
+                parsedArgs.Add("duration", float.PositiveInfinity);
+                parsedArgs.Add("loop", false);
+                parsedArgs.Add("allScreens", false);
+                for (int i = 0; i < Args.Length - 1; i++)
+                {
+                    switch (Args[i])
+                    {
+                        case "-m":
+                            parsedArgs["mute"] = true;
+                            break;
+                        case "-o":
+                            parsedArgs["opacity"] = float.Parse(Args[i + 1]);
+                            i++; // skip next arg as we just read it
+                            break;
+                        case "-d":
+                            parsedArgs["duration"] = float.Parse(Args[i + 1]);
+                            i++; // skip next arg as we just read it
+                            break;
+                        case "-l":
+                            parsedArgs["loop"] = true;
+                            break;
+                        case "-as":
+                            parsedArgs["allScreens"] = true;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace blueshell
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const uint WS_EX_TRANSPARENT = 0x00000020;
+        const int GWL_EXSTYLE = (-20);
+        const uint WS_EX_TOOLWINDOW = 0x00000080;
+        public System.DateTime t0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if ((DateTime.Now - t0).TotalSeconds > (float)App.parsedArgs["duration"])
+                Environment.Exit(0);
+        }   
+
+        private void Media_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            media.Position = TimeSpan.Zero;
+            media.Play();
+        }
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+        static extern uint GetWindowLongPtr(int hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+
+        public static void SetWindowExTransparent(IntPtr hwnd)
+        {
+            uint extendedStyle = GetWindowLongPtr(hwnd.ToInt32(), -20);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW);
+        }
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var hwnd = new WindowInteropHelper(this).Handle;
+            SetWindowExTransparent(hwnd);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            int pad = 16;
+            this.Width = System.Windows.SystemParameters.PrimaryScreenWidth + pad;
+            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight + pad;
+            this.Left = -pad / 2;
+            this.Top = -pad / 2;
+            this.Padding = new Thickness(0);
+            this.Margin = new Thickness(0);
+            this.Topmost = true;
+
+            try
+            {
+                media.Source = new System.Uri((string)App.parsedArgs["src"]);
+                media.IsMuted = (bool)App.parsedArgs["mute"];
+                media.Opacity = (float)App.parsedArgs["opacity"];
+                if ((bool)App.parsedArgs["loop"])
+                {
+                    media.MediaEnded += Media_MediaEnded;
+                }
+                t0 = DateTime.Now;
+                System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+                dispatcherTimer.Tick += dispatcherTimer_Tick;
+                dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+                dispatcherTimer.Start();
+            }
+            catch (Exception) { }
+        }
+    }
+}
